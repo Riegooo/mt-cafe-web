9fba791ab65fc2c2c014396830eb0b0893cadd52
Riegooo <christiandanielcagas0@gmail.com>
2026-05-04 18:39:37 +0800

    update

--- src/Javascript/landing_page.js
+++ src/Javascript/landing_page.js
@@ -15,3 +15,28 @@ menuTog.addEventListener("click", () => {
 });
 
 
+function submitForm() {
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    if (!name) {
+        alert("Please enter your name");
+        return;
+    } 
+    if (!email) {
+        alert("Please enter your email");
+        return;
+    } 
+    if (!message) {
+        alert("Please enter your Message");
+        return;
+    }
+
+    alert("Message sent successfully!");
+
+    document.getElementById('name').value  = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+
+}
