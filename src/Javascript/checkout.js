de2415ea30b63c0d9d2c1a15f4159f0930473b99
Riegooo <christiandanielcagas0@gmail.com>
2026-05-21 15:27:24 +0800

    add payment  method

--- src/Javascript/checkout.js
+++ src/Javascript/checkout.js
@@ -41,8 +41,75 @@ function displayUserCart() {
 
 }
 
+function popUpPaymentMethod() {
+
+    const placeOrderBtn = document.getElementById('placeOrder');
+    const paymentContainer = document.querySelector('.payment-container');
+    const xbutton = document.querySelector('.fa-solid.fa-xmark');
+
+    const continueBtn = document.getElementById('continueBtn');
+
+    if (!placeOrderBtn) return;
+
+    placeOrderBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const fullname = document.getElementById('fname').value.trim();
+        const phonenum = document.getElementById('phonenum').value.trim();
+        const orderType = document.querySelector('input[name="orderType"]:checked');
+
+        if (!fullname) {
+            alert("Enter your Full name");
+            return;
+        }
+
+        if (!phonenum){
+            alert("Enter your phone num");
+            return;
+        }
+
+        if (!orderType) {
+            alert("Choose Order Type");
+            return;
+        }
+
+        paymentContainer.classList.add("active");
+    });
+
+    continueBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const paymentMethod = document.querySelector('input[name="paymentMethod"]:checked');
+
+        if (!paymentMethod) {
+            alert("Select payment method");
+            return;
+        }
+
+        let userInfo = JSON.parse(localStorage.getItem("userInfo")) || [];
+
+        userInfo.push({
+            fullName: document.getElementById('fname').value.trim(),
+            phoneNum: document.getElementById('phonenum').value.trim(),
+            orderType: document.querySelector('input[name="orderType"]:checked').value,
+            paymentMethod: paymentMethod.value
+        });
+
+        localStorage.setItem("userInfo", JSON.stringify(userInfo));
+
+        window.location = '../pages/payment_page.html'
+
+        alert("Proceeding to payment...");
+    });
+
+    xbutton.addEventListener("click", (e) => {
+        e.preventDefault();
+        paymentContainer.classList.remove("active");
+    });
+
+}
 
 
-
+popUpPaymentMethod();
 
 displayUserCart();
